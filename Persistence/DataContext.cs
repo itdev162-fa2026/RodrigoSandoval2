020b0d9b2d16c6ea9f3dde33fd7f2349d7018c41
Rodrigo Sandoval <sandor14@matc.edu>
2025-11-18 20:38:56 -0600

    Complete Activity 9: Order Management Backend

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -7,18 +7,38 @@ namespace Persistence
   {
     public DbSet<WeatherForecast> WeatherForecasts { get; set; }
     public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+
     public string DbPath { get; }
 
-    public DataContext()
+        public DataContext()
     {
       var folder = Environment.SpecialFolder.LocalApplicationData;
       var path = Environment.GetFolderPath(folder);
       DbPath = System.IO.Path.Join(path, "Blogbox.db");
     }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-      options.UseSqlite($"Data Source={DbPath}");
+        optionsBuilder.UseSqlite("Data Source=blogbox.db");
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configure Order-OrderItem relationship
+        modelBuilder.Entity<Order>()
+            .HasMany(o => o.OrderItems)
+            .WithOne(oi => oi.Order)
+            .HasForeignKey(oi => oi.OrderId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+
+
+
+
+
   }
 }
